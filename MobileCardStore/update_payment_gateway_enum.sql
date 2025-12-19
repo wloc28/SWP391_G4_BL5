5492c0156975467fb955753fa3517b2f15dcb69e
Vũ Văn Lộc <locvvhe176805@fpt.edu.vn>
2025-12-19 19:56:55 +0700

    Remove update_payment_gateway_enum.sql from version control

--- MobileCardStore/update_payment_gateway_enum.sql
+++
@@ -1,6 +0,0 @@
--- Migration script: Add 'PAYOS' to payment_gateway_transactions.gateway_name enum
--- Run this SQL script if your database already exists and needs to be updated
-
-ALTER TABLE `payment_gateway_transactions` 
-MODIFY COLUMN `gateway_name` enum('VNPAY','MOMO','PAYOS') NOT NULL;
-
